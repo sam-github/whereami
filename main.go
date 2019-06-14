bcead504a11224833603cc4ce9d7a175e22fe2c0
Sam Roberts <vieuxtech@gmail.com>
2019-06-14 16:43:16 -0700

    Add arg parsing and usage

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
+var HELP = `usage: %s <tree-of-images>
+  Print to stdout location information for all images found
+  in 'tree-of-images'.
+`
+
 func main() {
-	fmt.Println("Where am I?")
+	if len(os.Args) != 2 {
+		fmt.Fprint(os.Stderr, HELP, os.Args[0])
+		os.Exit(2)
+	}
+	if os.Args[1] == "-h" || os.Args[1] == "-help" {
+		fmt.Fprintf(os.Stdout, HELP, os.Args[0])
+	} else {
+		fmt.Printf("tree-of-images: %s\n", os.Args[1])
+	}
 }
