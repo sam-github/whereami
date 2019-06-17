3b390cd3f63e36fbe56315e096b843c33b02edc7
Sam Roberts <vieuxtech@gmail.com>
2019-06-17 15:18:24 -0700

    Refactor to use channels
    Two channels needed, one for values, one for error.

--- walk.go
+++ walk.go
@@ -10,56 +10,117 @@ import (
 	exif "github.com/dsoprea/go-exif"
 )
 
-func walk(root string, out io.Writer) error {
+func files(root string) (<-chan string, <-chan error) {
+	ch := make(chan string)
+	cherr := make(chan error)
+
 	var walker = func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		if !info.Mode().IsRegular() {
-			return nil
+		if info.Mode().IsRegular() {
+			ch <- path
 		}
-
-		// Despite the declaration, this never return a non-nil error, it panics
-		// if anything goes wrong! It shouldn't do this, its unidiomatic AFAICT.
-		// I could open the file myself, slurp it in, and call
-		// SearchAndExtractExif (which doesn't panic), but recover also works,
-		// and avoids having to copy-n-paste in the file slurping code.
-		defer func() {
-			recover()
-		}()
-		rawExif, err := exif.SearchFileAndExtractExif(path)
-		if err != nil {
-			return nil // File had no exif, ignore.
-		}
-		im := exif.NewIfdMapping()
-
-		err = exif.LoadStandardIfds(im)
-		if err != nil {
-			log.Panic(err) // go-exif can't load its internals, fatal.
-		}
-
-		ti := exif.NewTagIndex()
-
-		_, index, err := exif.Collect(im, ti, rawExif)
-		if err != nil {
-			return nil // Stuff that looked like exif wasn't, ignore.
-		}
-
-		ifd, err := index.RootIfd.ChildWithIfdPath(exif.IfdPathStandardGps)
-		if err != nil {
-			return nil // No GPS info, ignore
-		}
-
-		gi, err := ifd.GpsInfo()
-		if err != nil {
-			return nil // Stuff that is supposed to be GPS info wasn't, ignore.
-		}
-
-		fmt.Fprintf(out, "%q,%v,%v\n",
-			path, gi.Latitude.Decimal(), gi.Longitude.Decimal())
 
 		return nil
 	}
-	return filepath.Walk(root, walker)
+
+	go func() {
+		defer close(ch)
+		defer close(cherr)
+		err := filepath.Walk(root, walker)
+		if err != nil {
+			cherr <- err
+		}
+	}()
+
+	return ch, cherr
+}
+
+type LatLong struct {
+	path      string
+	latitude  float64
+	longitude float64
+}
+
+func extract(file string) (ll LatLong, ok bool) {
+	// Despite the declaration, exif.SearchFileAndExtractExif never returns
+	// a non-nil error, it panics if anything goes wrong! It shouldn't do
+	// this, its unidiomatic AFAICT.  I could open the file myself, slurp it
+	// in, and call SearchAndExtractExif (which doesn't panic), but recover
+	// also works, and avoids having to copy-n-paste in the file slurping
+	// code.
+	defer func() {
+		recover()
+	}()
+	rawExif, err := exif.SearchFileAndExtractExif(file)
+	if err != nil {
+		return
+	}
+	im := exif.NewIfdMapping()
+
+	err = exif.LoadStandardIfds(im)
+	if err != nil {
+		log.Panic(err) // go-exif can't load its internals, just die.
+	}
+
+	ti := exif.NewTagIndex()
+
+	_, index, err := exif.Collect(im, ti, rawExif)
+	if err != nil {
+		return // Stuff that looked like exif wasn't, ignore.
+	}
+
+	ifd, err := index.RootIfd.ChildWithIfdPath(exif.IfdPathStandardGps)
+	if err != nil {
+		return // No GPS info, ignore
+	}
+
+	gi, err := ifd.GpsInfo()
+	if err != nil {
+		return // Stuff that is supposed to be GPS info wasn't, ignore.
+	}
+
+	return LatLong{file, gi.Latitude.Decimal(), gi.Longitude.Decimal()}, true
+}
+
+func latlong(files <-chan string, errors <-chan error) (
+	<-chan LatLong, <-chan error) {
+	ch := make(chan LatLong)
+	cherr := make(chan error)
+	go func() {
+		defer close(ch)
+		defer close(cherr)
+		for {
+			select {
+			case err := <-errors:
+				cherr <- err
+				return // Fail fast and pass it on
+			case file := <-files:
+				if ll, ok := extract(file); ok {
+					ch <- ll
+				}
+			}
+		}
+	}()
+	return ch, cherr
+}
+
+func csv(ll <-chan LatLong, errors <-chan error, out io.Writer) error {
+	for {
+		select {
+		case err := <-errors:
+			return err
+		case exif := <-ll:
+			fmt.Fprintf(out, "%q,%v,%v\n",
+				exif.path, exif.latitude, exif.longitude)
+		}
+	}
+}
+
+func walk(root string, out io.Writer) error {
+	f, cherr := files(root)
+	l, cherr := latlong(f, cherr)
+	return csv(l, cherr, out)
 }
